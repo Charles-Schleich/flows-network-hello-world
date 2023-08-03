5c7af099fdba6aa583477ddb20f1b38365a7b86a
Charles Schleich <schleich.cdaniel@gmail.com>
2023-08-04 00:42:41 +0200

    Writing formatted Image out response once fractal has been generated

--- src/lib.rs
+++ src/lib.rs
@@ -1,48 +1,47 @@
 use flowsnet_platform_sdk::logger;
 use lambda_flows::{request_received, send_response};
 
+use image::{write_buffer_with_format, ColorType, EncodableLayout, ImageOutputFormat};
 use serde_json::Value;
 use std::collections::HashMap;
+use std::io::{BufWriter, Cursor};
 
 #[no_mangle]
 #[tokio::main(flavor = "current_thread")]
 pub async fn run() -> anyhow::Result<()> {
-
-    request_received(|headers, qry: HashMap<String, Value>, body| handler(headers, qry, body)).await;
+    request_received(|headers, qry: HashMap<String, Value>, body| handler(headers, qry, body))
+        .await;
     Ok(())
 }
 
-async fn handler(
-    headers: Vec<(String, String)>,
-    qry: HashMap<String, Value>,
-    _body: Vec<u8>,
-) {
+async fn handler(headers: Vec<(String, String)>, _qry: HashMap<String, Value>, _body: Vec<u8>) {
     logger::init();
     log::info!("Headers -- {:?}", headers);
 
-    let msg = qry.get("msg").unwrap();
+    // let msg = qry.get("msg").unwrap();
 
-    let resp = format!(
-        "Testing Flows Network: This is your message {msg}"
-    );
+    // let resp = format!("Testing Flows Network: This is your message {msg}");
+    let fractal_bytes = match generate_fractal(){
+        Ok(bytes) => bytes,
+        Err(err) => err.as_bytes().to_vec(),
+    };
 
     send_response(
         200,
         vec![(String::from("content-type"), String::from("text/html"))],
-        resp.as_bytes().to_vec(),
+        fractal_bytes,
     );
 }
 
-use image::{ImageBuffer, EncodableLayout};
-fn generate_fractal(){
-    let imgx = 800;
-    let imgy = 800;
+fn generate_fractal() -> Result<Vec<u8>, String> {
+    const IMG_X: u32 = 800;
+    const IMG_Y: u32 = 800;
 
-    let scalex = 3.0 / imgx as f32;
-    let scaley = 3.0 / imgy as f32;
+    let scalex = 3.0 / IMG_X as f32;
+    let scaley = 3.0 / IMG_Y as f32;
 
     // Create a new ImgBuf with width: imgx and height: imgy
-    let mut imgbuf = image::ImageBuffer::new(imgx, imgy);
+    let mut imgbuf = image::ImageBuffer::new(IMG_X, IMG_Y);
 
     // Iterate over the coordinates and pixels of the image
     for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
@@ -52,8 +51,8 @@ fn generate_fractal(){
     }
 
     // A redundant loop to demonstrate reading image data
-    for x in 0..imgx {
-        for y in 0..imgy {
+    for x in 0..IMG_X {
+        for y in 0..IMG_Y {
             let cx = y as f32 * scalex - 1.5;
             let cy = x as f32 * scaley - 1.5;
 
@@ -72,6 +71,20 @@ fn generate_fractal(){
         }
     }
 
-    let image_buff_bytes = imgbuf.as_bytes().to_vec();
+    let buffer = [0u8; IMG_X as usize * IMG_Y as usize];
+    let cursor_buffer = Cursor::new(buffer); // needed to implement Seek
+    let mut stream = BufWriter::new(cursor_buffer);
+    
+    match write_buffer_with_format(
+        &mut stream,
+        imgbuf.as_bytes(),
+        IMG_X,
+        IMG_Y,
+        ColorType::Rgb8,
+        ImageOutputFormat::Png,
+    ){
+        Ok(_) => Ok(stream.buffer().to_vec()),
+        Err(err) => Err(format!("Error writing Image {err}")),
+    }
 
 }
