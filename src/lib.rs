65df413dcd410a3e92215027e4ab299af0b925d2
Charles Schleich <schleich.cdaniel@gmail.com>
2023-08-03 23:25:13 +0200

    added API call to expose ip address of lambda_flows server

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,40 @@
-use lambda_flows::{request_received, send_response};
 use flowsnet_platform_sdk::logger;
-use std::collections::HashMap;
+use lambda_flows::{request_received, send_response};
+use reqwest::{self, Client};
 use serde_json::Value;
+use std::collections::HashMap;
 
 #[no_mangle]
 #[tokio::main(flavor = "current_thread")]
 pub async fn run() -> anyhow::Result<()> {
-    request_received(|headers, qry, body| {
-        handler(headers, qry, body)
-    }).await;
+    let client = reqwest::Client::new();
+
+    request_received(|headers, qry, body| handler(headers, qry, body, client)).await;
     Ok(())
 }
+const IP_API: &str = "api.ipify.org";
 
-async fn handler(headers: Vec<(String, String)>, qry: HashMap<String, Value>, _body: Vec<u8>) {
+async fn handler(
+    headers: Vec<(String, String)>,
+    qry: HashMap<String, Value>,
+    _body: Vec<u8>,
+    client: Client,
+) {
     logger::init();
     log::info!("Headers -- {:?}", headers);
 
     let msg = qry.get("msg").unwrap();
-    // let msg = String::from_utf8(body).unwrap_or("".to_string());
-    let resp = format!("Welcome to flows.network.\nYou just said: '{}'.\nLearn more at: https://github.com/flows-network/hello-world\n", msg);
+    let ip_resp = match client.get(IP_API).send().await {
+        Ok(resp) => match resp.text().await {
+            Ok(ok) => ok,
+            Err(err) => format!("Could not decode response from {IP_API} to text {err}"),
+        },
+        Err(err) => format!("Request to {IP_API} Failed {}", err),
+    };
+
+    let resp = format!(
+        "Welcome to flows.network.\n Echo Message: {msg}\nResult from internal IP Fetch :{ip_resp}"
+    );
 
     send_response(
         200,
