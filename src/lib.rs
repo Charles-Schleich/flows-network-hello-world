baba87f6bc9736d9a81498db7f1e8f45beceb499
Charles Schleich <schleich.cdaniel@gmail.com>
2023-08-03 23:52:21 +0200

    removed reqwest dep as it is not on allowed list

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,16 @@
 use flowsnet_platform_sdk::logger;
 use lambda_flows::{request_received, send_response};
-use reqwest::{self, Client};
+
 use serde_json::Value;
 use std::collections::HashMap;
 
 #[no_mangle]
 #[tokio::main(flavor = "current_thread")]
 pub async fn run() -> anyhow::Result<()> {
-    let client = reqwest::Client::new();
 
-    request_received(|headers, qry, body| handler(headers, qry, body, client)).await;
+    request_received(|headers, qry: HashMap<String, Value>, body| handler(headers, qry, body, client)).await;
     Ok(())
 }
-const IP_API: &str = "api.ipify.org";
 
 async fn handler(
     headers: Vec<(String, String)>,
@@ -24,16 +22,9 @@ async fn handler(
     log::info!("Headers -- {:?}", headers);
 
     let msg = qry.get("msg").unwrap();
-    let ip_resp = match client.get(IP_API).send().await {
-        Ok(resp) => match resp.text().await {
-            Ok(ok) => ok,
-            Err(err) => format!("Could not decode response from {IP_API} to text {err}"),
-        },
-        Err(err) => format!("Request to {IP_API} Failed {}", err),
-    };
 
     let resp = format!(
-        "Welcome to flows.network.\n Echo Message: {msg}\nResult from internal IP Fetch :{ip_resp}"
+        "Testing Flows Network: This is your message {msg}"
     );
 
     send_response(
